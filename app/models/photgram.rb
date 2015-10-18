0487ed0cd6feb880b09519c9aefdbfa946be1e7c
Henry Arbolaez <henryarbolaez@gmail.com>
2015-10-18 00:16:00 -0400

    add index layout

--- app/models/photgram.rb
+++ app/models/photgram.rb
@@ -1,5 +1,5 @@
 class Photgram < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, styles: { medium: "200x300#" }
+	has_attached_file :image, styles: { medium: "300x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
