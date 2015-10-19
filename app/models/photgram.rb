9652964da17109cd81980cc6c4ade84cf7ee58fb
Henry Arbolaez <henryarbolaez@gmail.com>
2015-10-18 22:39:36 -0400

    Add username to Devise

--- app/models/photgram.rb
+++ app/models/photgram.rb
@@ -2,4 +2,5 @@ class Photgram < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "300x400#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_presence_of :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :photgrams
 
-
   root to: "photgrams#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
